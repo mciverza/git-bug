310d75983b621192eadace0b2d9b96a1f3cb9702
Michael Muré <batolettre@gmail.com>
2020-11-08 19:15:06 +0100

    bug: Id from first operation data, not git + remove root link

--- identity/identity_test.go
+++ identity/identity_test.go
@@ -6,7 +6,6 @@ import (
 
 	"github.com/stretchr/testify/require"
 
-	"github.com/MichaelMure/git-bug/entity"
 	"github.com/MichaelMure/git-bug/repository"
 	"github.com/MichaelMure/git-bug/util/lamport"
 )
